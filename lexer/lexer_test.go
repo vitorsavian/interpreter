bcdd9a76184adeaccb703a3adf9b30f453e1ef45
Vitor Savian <vitor.savian@suse.com>
2023-11-21 00:31:09 -0300

    Added new keywords

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -18,7 +18,12 @@ func TestNextToken(t *testing.T) {
 	let result = add(five, ten);
 	!-/*5;
    	5 < 10 > 5;
-	`
+
+	if (5 < 10) {
+		return true;
+	} else {
+		return false;
+	}`
 
 	tests := []struct {
 		expectedType    token.TokenType
--- token/token.go
+++ token/token.go
@@ -20,6 +20,11 @@ const (
 	// Keywords
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 
 	// OPERATORS
 	ASSIGN   = "="
@@ -41,8 +46,13 @@ type Token struct {
 }
 
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 func LookupIdent(ident string) TokenType {
